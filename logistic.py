4a93f0e48ca58485ebd883c7c2bf3d9e3942526d
mikelpri3 <144795034+mikelpri3@users.noreply.github.com>
2025-03-12 17:43:47 +0100

    Pruebas logistic

--- logistic.py
+++ logistic.py
@@ -14,7 +14,13 @@ dataset_path = os.path.join(dataset_path, "Data")
 x, y = load_dataset(dataset_path)
 X_train, X_test, y_train, y_test = load_dataset(dataset_path)
 
-model = LogisticRegression()
+# Flatten the images for Logistic Regression
+X_train = X_train.reshape(X_train.shape[0], -1)  # [num_samples, num_features]
+X_test = X_test.reshape(X_test.shape[0], -1)  # [num_samples, num_features]
+
+# Initialize Logistic Regression for Multiclass Classification
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=500)
+#model = LogisticRegression()
 model.fit(X_train, y_train)
 
 #Predictions
