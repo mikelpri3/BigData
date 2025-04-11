feb6ef18b9693a28c24f55e508992164c1315aff
alvaroresano <144225701+alvaroresano@users.noreply.github.com>
2025-04-11 12:47:03 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -2,78 +2,81 @@ import kagglehub
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import torch 
+from torch.utils.data import Dataset
+
+dataset_path = kagglehub.dataset_download("mohammadhossein77/brain-tumors-dataset")
+dataset_path = os.path.join(dataset_path, "Data") #Enter in the folder Data
+
 
 IMG_SIZE = (224, 224)
 
-#funcion para cargar imagen
-def load_and_preprocess_image(image_path):
-  img = cv2.imread(image_path)
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  img = cv2.resize(img, IMG_SIZE)
-  img = img / 255.0  # Normalizar valores entre 0 y 1
-  return img
-
-def load_dataset(path):
-  #Cargar path a carpetas
-  healthy_path = os.path.join(path, "Normal")
-  tumor_path = os.path.join(path, "Tumor")
-
-  #Cargar path carpetas tumores
-  glioma_path = os.path.join(tumor_path, "glioma_tumor")
-  meningioma_path = os.path.join(tumor_path, "meningioma_tumor")
-  pituitary_path = os.path.join(tumor_path, "pituitary_tumor")
-
-  #Cargar imagenes
-  healthy_images = os.listdir(healthy_path)
-  glioma_images = os.listdir(glioma_path)
-  meningioma_images = os.listdir(meningioma_path)
-  pituitary_images = os.listdir(pituitary_path)
-
-  # max_images = 500  # Ajusta según la memoria disponible para ejecutarlo en el portatil
-  # healthy_images = healthy_images[:max_images]
-  # glioma_images = glioma_images[:max_images]
-  # meningioma_images = meningioma_images[:max_images]
-  # pituitary_images = pituitary_images[:max_images]
-
-  x = [] #imagenes
-  y = [] #etiquetas
-
-  #Pasar las imagenes a arrays
-  for img_name in healthy_images:
-    img_path = os.path.join(healthy_path, img_name)
-    x.append(load_and_preprocess_image(img_path))
-    y.append(0)
-
-  for img_name in glioma_images:
-    img_path = os.path.join(glioma_path, img_name)
-    x.append(load_and_preprocess_image(img_path))
-    y.append(1)
-
-  for img_name in meningioma_images:
-    img_path = os.path.join(meningioma_path, img_name)
-    x.append(load_and_preprocess_image(img_path))
-    y.append(2)
-
-  for img_name in pituitary_images:
-    img_path = os.path.join(pituitary_path, img_name)
-    x.append(load_and_preprocess_image(img_path))
-    y.append(3)
-
-  #convertir a array de numpy
-  x = np.array(x)
-  y = np.array(y)
-
-  # Separar en entrenamiento y prueba
-  return train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True)
 
 
-# Si ejecutamos este archivo directamente, cargamos el dataset (codigo para hacer el main)
-if __name__ == "__main__":
-  dataset_path = kagglehub.dataset_download("mohammadhossein77/brain-tumors-dataset")
-  dataset_path = os.path.join(dataset_path, "Data")
-  X_train, X_test, y_train, y_test = load_dataset(dataset_path)
-  print(f"Datos cargados: {len(X_train)} entrenamiento, {len(X_test)} prueba")
+#Function to differenciate images with axial perspective
+def is_axial_based_on_black_border(img_path, border_thickness=10, black_thresh=10, black_ratio_thresh=0.99):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        return False
+    img = cv2.resize(img, (224, 224))
 
+    # Extract borders
+    top = img[:border_thickness, :]
+    bottom = img[-border_thickness:, :]
+    left = img[:, :border_thickness]
+    right = img[:, -border_thickness:]
 
+    # Unify all borders
+    border_pixels = np.concatenate((top.flatten(), bottom.flatten(), left.flatten(), right.flatten()))
+    
+    # Compute proportion of black pixels
+    black_pixels = np.sum(border_pixels < black_thresh)
+    black_ratio = black_pixels / len(border_pixels)
+
+    return black_ratio > black_ratio_thresh
+
+###########################
+
+#Dataset declaration
+class BrainTumorDataset(Dataset):
+    def __init__(self, root_dir, transform=None, view_type="axial"):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.view_type = view_type
+        self.classes = ["Normal", "Tumor/glioma_tumor", "Tumor/meningioma_tumor", "Tumor/pituitary_tumor"]
+        self.images = []
+        self.labels = []
+
+        for label, category in enumerate(self.classes):
+            category_path = os.path.join(root_dir, category)
+            for img_name in os.listdir(category_path):
+                img_path = os.path.join(category_path, img_name)
+                if self.view_type == "all":
+                    self.images.append(img_path)
+                    self.labels.append(label)
+                     
+                if is_axial_based_on_black_border(img_path) and self.view_type == "axial":
+                                    self.images.append(img_path)
+                                    self.labels.append(label)
+                elif not is_axial_based_on_black_border(img_path) and self.view_type == "sagittal":
+                                    self.images.append(img_path)
+                                    self.labels.append(label)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = self.images[idx]
+        img = cv2.imread(img_path)
+        if img is None:
+            raise FileNotFoundError(f"Could not load image: {img_path}")
+        img = cv2.resize(img, IMG_SIZE)
+        img = img / 255.0
+        img = np.transpose(img, (2, 0, 1))
+        img = torch.tensor(img, dtype=torch.float32)
+        label = self.labels[idx]
+        return img, label
+    
+brain_dataset = BrainTumorDataset(dataset_path, view_type="all") #load all images
+
+axial_dataset = BrainTumorDataset(dataset_path, view_type="axial") #load axial images
